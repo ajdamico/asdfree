572b66d5792adf0ed0f8091d71ce242bacd7124f
Anthony Damico <ajdamico@gmail.com>
2012-09-19 07:45:05 -0400

    altered summing lines

--- Current Population Survey/2012 asec - analysis examples.R
+++ Current Population Survey/2012 asec - analysis examples.R
@@ -117,7 +117,10 @@ svytotal(
 # note that this is exactly equivalent to summing up the weight variable
 # from the original cps data frame
 
-sum( x$marsupwt )
+db <- dbConnect( SQLite() , "cps.asec.2012.db" )	# connect to the SQLite database (.db)
+dbGetQuery( db , 'select sum( marsupwt ) from x' )	# run a single query, summing the person-weight
+dbDisconnect( db )									# disconnect from the database
+
 
 # the civilian, non-institutionalized population of the united states #
 # by employment status
