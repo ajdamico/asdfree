9eaa8862a308305d3511e9cc7fe5ad10298f47bb
Anthony Damico <ajdamico@gmail.com>
2013-06-10 17:25:07 -0400

    taxonomy merge example plus replication improvement

---
+++ National Plan and Provider Enumeration System/merge taxonomy ids.R
@@ -0,0 +1,281 @@
+# analyze us government survey data with the r language
+# national plan and provider enumeration system files
+
+# if you have never used the r language before,
+# watch this two minute video i made outlining
+# how to run this script from start to finish
+# http://www.screenr.com/Zpd8
+
+# anthony joseph damico
+# ajdamico@gmail.com
+
+# if you use this script for a project, please send me a note
+# it's always nice to hear about how people are using this stuff
+
+# for further reading on cross-package comparisons, see:
+# http://journal.r-project.org/archive/2009-2/RJournal_2009-2_Damico.pdf
+
+
+# this r script will merge the taxonomy id (specialty code) table available at:
+# https://raw.github.com/ajdamico/usgsd/master/National%20Plan%20and%20Provider%20Enumeration%20System/taxonomy%20id%20table.txt
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+###################################################################################################################################
+# prior to running this analysis script, the national plan and provider enumeration system must be imported into a monet database #
+# on the local machine. you must run this:                                                                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# https://raw.github.com/ajdamico/usgsd/master/National%20Plan%20and%20Provider%20Enumeration%20System/download%20and%20import.R  #                                        #
+###################################################################################################################################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# # # # # # # # # # # # # # #
+# warning: monetdb required #
+# # # # # # # # # # # # # # #
+
+
+# remove the # in order to run this install.packages line only once
+# install.packages( c( "stringr" , "downloader" ) )
+
+
+require(MonetDB.R)	# load the MonetDB.R package (connects r to a monet database)
+require(stringr)	# load stringr package (manipulates character strings easily)
+require(downloader)	# downloads and then runs the source() function on scripts from github
+
+
+# create a temporary file on the local disk
+tf <- tempfile()
+
+# write the taxonomy id table to that temporary file
+download( 
+	"https://raw.github.com/ajdamico/usgsd/master/National%20Plan%20and%20Provider%20Enumeration%20System/taxonomy%20id%20table.txt" ,
+	tf
+)
+
+# read that taxonomy id table into R
+z <- readLines( tf )
+
+# search for tab characters on each line
+hmt <- gregexpr( "\t" , z )
+# since the gregexpr() function returns `-1` for non-matches..
+
+# ..construct an in-line function that only counts non-negative hits
+
+# for each line, count the number of '\t' matches
+l <- 
+	unlist( 
+		lapply( 
+			hmt , 
+			function( x ) length( x[ x > 0 ] ) 
+		) 
+	)
+
+# remove all tabs from the object `z`
+z <- gsub( '\t' , '' , z )
+	
+
+# find the last dash in each string
+ld <- lapply( gregexpr( "-" , z ) , max )
+
+# replace all strings without dashes with their last character
+ld[ ld < 0 ] <- nchar( z[ ld < 0 ] )
+
+# coerce this list object of last-dash positions to a numeric vector
+ld <- unlist( ld )
+
+# for all lines in `z`, take the first character thru the last dash
+pre <- substr( z , 1 , ld )
+
+# if `pre` ends with a dash, trim it.
+# first, determine elements where the last character is a dash..
+ewd <- substr( pre , nchar( pre ) , nchar( pre ) ) == "-"
+
+# ..then, for those records, take a substring up to the character before the last.
+pre[ ewd ] <- 
+	substr( 
+		pre[ ewd ] , 
+		1 , 
+		nchar( pre[ ewd ] ) - 1 
+	)
+
+# remove whitespace on both sides of the string
+pre <- str_trim( pre )
+	
+# for the post-dash portion of our show,
+# just take the last dash plus one to the end of the string.
+post <- substr( z , ld + 1 , nchar( z ) )
+
+# remove whitespace here too
+post <- str_trim( post )
+
+# replace missing data ("") with NA
+post[ post == "" ] <- NA
+
+# create a data.frame object with three columns.
+# the level (1, 2, or 3)
+# the taxonomy id title
+# the actual taxonomy id or code
+w <-
+	data.frame(
+		level = l ,
+		title = pre ,
+		taxonomy.id = post
+	)
+
+
+# remove the top level (level zero)
+w <- w[ w$level != 0 , ]
+
+
+# set two current level variables to missing
+curLevel.1 <- curLevel.2 <- NA
+
+
+# cycle through every record in the taxonomy id table..
+for ( i in seq( nrow( w ) ) ){
+
+	# if the record is level one, store that in the curLevel.1 variable
+	if ( w[ i , 'level' ] == 1 ) curLevel.1 <- w[ i , 'title' ]
+
+	# if the record is level two, store that in the curLevel.2 variable
+	# and also record the current level one
+	if ( w[ i , 'level' ] == 2 ) {
+		w[ i , 'level.one' ] <- curLevel.1
+		curLevel.2 <- w[ i , 'title' ]
+	}
+	
+	# if the record is level three, record both current levels one and two
+	if ( w[ i , 'level' ] == 3 ) {
+		w[ i , 'level.one' ] <- curLevel.1
+		w[ i , 'level.two' ] <- curLevel.2
+	}
+	
+}
+
+# throw out missing taxonomy ids
+w <- w[ !is.na( w$taxonomy.id ) , ]
+
+# now the object `w` contains all taxonomy ids
+# take a look at the top and bottom of this data.frame
+head( w ) ; tail( w )
+
+
+# after running the r script above, users should have handy a few lines
+# to initiate and connect to the monet database containing the latest
+# national plan and provider enumeration system table.  run them now.  mine look like this:
+
+
+
+####################################################################
+# lines of code to hold on to for all other nppes monetdb analyses #
+
+# first: specify your batfile.  again, mine looks like this:
+# uncomment this line by removing the `#` at the front..
+# batfile <- "C:/My Directory/NPPES/nppes.bat"
+
+# second: run the MonetDB server
+pid <- monetdb.server.start( batfile )
+
+# third: your five lines to make a monet database connection.
+# just like above, mine look like this:
+dbname <- "nppes"
+dbport <- 50006
+
+monet.url <- paste0( "monetdb://localhost:" , dbport , "/" , dbname )
+db <- dbConnect( MonetDB.R() , monet.url )
+
+# end of lines of code to hold on to for all other nppes monetdb analyses #
+###########################################################################
+
+
+
+# now R has connected to the MonetDB
+# seen in the command below stored as the `db` object
+
+
+# perform a simple count directly from the sql table
+# stored inside the monet database on your hard disk
+# (as opposed to RAM)
+dbGetQuery( db , 'select count(*) from npi' )
+
+
+# look at all tables in the monet database
+dbListTables( db )
+# note: `npi` is the only table that's not administrative
+# all other tables shown here come standard with MonetDB
+
+# look at all of the field names of the `npi` table
+dbListFields( db , 'npi' )
+# note: this is similar to running the names()
+# function on an R data.frame object
+
+
+# run a more complex sql SELECT query
+# that extracts practice location state and
+# the first (of many!) taxonomy id codes
+
+# the results are saved into a new data.frame object `ftcbs`..
+
+# define which columns to keep
+vars.to.keep <-
+	c(
+		"healthcare_provider_taxonomy_code_1" ,
+		"provider_business_practice_location_address_state_name"
+	)
+
+# pull an R data.frame object from the monet database
+ftcbs <-
+	dbGetQuery( 
+		db , 
+		paste( 
+			'select' , 
+			paste( vars.to.keep , collapse = "," ) ,
+			'from npi'
+		)
+	)
+
+# remove records missing their first taxonomy code
+ftcbs <- ftcbs[ ftcbs$healthcare_provider_taxonomy_code_1 != '' , ]
+
+# merge this first taxonomy code by state table
+# with the `w` taxonomy code table
+# into a new `v` data.frame object
+v <- 
+	merge( 
+		ftcbs , 
+		w , 
+		by.x = 'healthcare_provider_taxonomy_code_1' , 
+		by.y = 'taxonomy.id' 
+	)
+	
+# confirm that every record has exactly one match
+stopifnot( nrow( ftcbs ) == nrow( v ) )
+
+
+# look at the first six records
+head( v )
+
+# look at the last six records
+tail( v )
+
+# and hey, look at currently active providers in california
+table( v[ v$provider_business_practice_location_address_state_name == 'CA' , 'title' ] , useNA = 'always' )
+# neato.
+
+
+###########################################################################
+# end of lines of code to hold on to for all other nppes monetdb analyses #
+
+# disconnect from the current monet database
+dbDisconnect( db )
+
+# and close it using the `pid`
+monetdb.server.stop( pid )
+
+# end of lines of code to hold on to for all other nppes monetdb analyses #
+###########################################################################
+
+# for more details on how to work with data in r
+# check out my two minute tutorial video site
+# http://www.twotorials.com/
--- National Plan and Provider Enumeration System/replicate cms state counts.R
+++ National Plan and Provider Enumeration System/replicate cms state counts.R
@@ -16,8 +16,28 @@
 # http://journal.r-project.org/archive/2009-2/RJournal_2009-2_Damico.pdf
 
 
-# this r script will replicate the counts table provided by
-# the centers for medicare and medicaid services (cms)
+# this r script will create the state x count table available at:
+# https://raw.github.com/ajdamico/usgsd/master/National%20Plan%20and%20Provider%20Enumeration%20System/replication%20of%20CMS-provided%20state%20counts%20from%20PUF.csv
+
+# which replicated the "May 2013" counts table provided to me by
+# the centers for medicare and medicaid services (cms), available at:
+# https://github.com/ajdamico/usgsd/blob/master/National%20Plan%20and%20Provider%20Enumeration%20System/Public%20File%20May%202013.xlsx?raw=true
+
+# here's some additional detail from the folks at cms regarding this file:
+#
+# This spreadsheet will contain three tabs. 
+# 1. Data in the first tab is provided from the public file which has the breakdown by state.
+#	The state for ‘54847’ records are recorded as empty.
+#	These ‘54847’ records are the deactivated NPIs.
+# 2. Second tab has the data for the ‘54847’ deactivated NPIs with state breakdown.
+#	This data was taken from NPPES Production database.
+# 3. Third tab is the result of combining data from tabs 1 and 2, which the user is interested in.
+#
+# Notes:
+# 1. Outside analysts cannot reproduce the data provided in the second/third tab
+#	as the business requirement only allows limited data for the deactivated records.
+# 2. If the users want to categorize these deactivated NPIs (Tab2 data) by deactivation date,
+#	they can use the column "NPI Deactivation Date" as a filter only for these records.
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -67,15 +87,122 @@ db <- dbConnect( MonetDB.R() , monet.url )
 
 
 
+# now R has connected to the MonetDB
+# seen in the command below stored as the `db` object
 
+
+# perform a simple count directly from the sql table
+# stored inside the monet database on your hard disk
+# (as opposed to RAM)
+dbGetQuery( db , 'select count(*) from npi' )
+
+
+# look at all tables in the monet database
+dbListTables( db )
+# note: `npi` is the only table that's not administrative
+# all other tables shown here come standard with MonetDB
+
+# look at all of the field names of the `npi` table
+dbListFields( db , 'npi' )
+# note: this is similar to running the names()
+# function on an R data.frame object
+
+
+# run a more complex sql SELECT query
+# that provides counts of practice locations by state
+# ordered by the number of records
+
+# the results are saved into a new data.frame object `z`..
+z <- 
+	dbGetQuery( 
+		db , 
+		'select 
+			provider_business_practice_location_address_state_name , 
+			count(*) as count 
+		from 
+			npi 
+		group by 
+			provider_business_practice_location_address_state_name 
+		order by 
+			count 
+			desc' 
+	)
+
+
+# ..which can then be viewed immediately..
+
+# look at the first six records
+head( z )
+
+# look at the last six records
+tail( z )
+
+# ..or exported as a comma-separated value file 
+# into your current working directory 
+write.csv( z , "counts by state.csv" )
+	
 
 
 # # # # # # # # # # # # # # # #
 # create a monet.frame object #
 # # # # # # # # # # # # # # # #
 
+# initiate a monet.frame object,
+# which in many ways behaves
+# like an R data.frame
+x <- monet.frame( db , 'npi' )
+# for more detail about and
+# example usage cases of monet.frame objects,
+# type ?monet.frame into the console
 
 
+# note: the entire nppes data table is too large to entirely load onto a computer with 4GB of RAM
+# however, pulling only certain columns into your computer's RAM at once should load properly
+
+
+
+# extraction based on column _numbers_ #
+
+# create an R data.frame object `y` from the monet.frame object `x`
+# pulling the first ten columns of the data table
+# and removing the RAM-related warning.
+y <- 
+	as.data.frame( 
+		x[ , c( 1:2 , 31:32 , 37 ) ] , 
+		warnSize = FALSE 
+	)
+
+# from here, a table comparable to the object `z` above
+# can simply be printed directly to the screen
+# using the base R `table` function
+table( y$provider_business_practice_location_address_state_name )
+
+# remove `y` from RAM
+rm( y ) ; gc()
+
+
+# extraction based on column _names_ #
+
+vars.to.keep <- 
+	c( 'npi' , 'entity_type_code' , 'provider_business_practice_location_address_city_name' ,
+		'provider_business_practice_location_address_state_name' , 'provider_enumeration_date' )
+
+# create an R data.frame object `y` from the monet.frame object `x`
+# pulling the first ten columns of the data table
+# and removing the RAM-related warning.
+y <- 
+	as.data.frame( 
+		x[ , vars.to.keep ] , 
+		warnSize = FALSE 
+	)
+
+# from here, a table comparable to the object `z` above
+# can simply be printed directly to the screen
+# using the base R `table` function
+table( y$provider_business_practice_location_address_state_name )
+
+# remove `y` from RAM
+rm( y ) ; gc()
 
 
 ###########################################################################
