2d2a93e24d6ab229581b400959b415ec88e4dc4a
Anthony Damico <ajdamico@gmail.com>
2013-06-21 12:21:45 -0400

    structure improvement

--- MonetDB/monet.read.tsv.R
+++ MonetDB/monet.read.tsv.R
@@ -3,21 +3,41 @@
 # which converts ascii files to tab-separated files.  this new monet.read.tsv will read in those straightaway!
 
 monet.read.tsv <-
-	function ( conn , files , tablename , nrows , header = TRUE , locked = FALSE , na.strings = "" , nrow.check = 500 ) {
+	function ( 
+		conn , 
+		files , 
+		tablename , 
+		nrows , header = TRUE , 
+		locked = FALSE , 
+		na.strings = "" , 
+		nrow.check = 500 ,
+		structure = NULL
+	) {
     
 	if (length(na.strings) > 1) stop("na.strings must be of length 1")
-    
-	headers <- lapply(files, read.table, header = TRUE , sep = '\t' , na.strings = "NA", nrows = nrow.check)
-    
-	if (length(files) > 1) {
-        nn <- sapply(headers, ncol)
-        if (!all(nn == nn[1])) stop("Files have different numbers of columns")
-        nms <- sapply(headers, names)
-        if (!all(nms == nms[, 1])) stop("Files have different variable names")
-        types <- sapply(headers, function(df) sapply(df, dbDataType, dbObj = conn))
-        if (!all(types == types[, 1])) stop("Files have different variable types")
-    }
-    dbWriteTable(conn, tablename, headers[[1]][FALSE, ])
+
+	# if a structure file is passed in
+	if ( !is.null( structure ) ){
+		dbWriteTable(conn, tablename, structure[FALSE, ])
+	} else {
+	
+		# otherwise, determine headers yourself
+		
+		headers <- lapply(files, read.table, header = TRUE , sep = '\t' , na.strings = "NA", nrows = nrow.check)
+		
+		if (length(files) > 1) {
+			nn <- sapply(headers, ncol)
+			if (!all(nn == nn[1])) stop("Files have different numbers of columns")
+			nms <- sapply(headers, names)
+			if (!all(nms == nms[, 1])) stop("Files have different variable names")
+			types <- sapply(headers, function(df) sapply(df, dbDataType, dbObj = conn))
+			if (!all(types == types[, 1])) stop("Files have different variable types")
+		}
+	
+		dbWriteTable(conn, tablename, headers[[1]][FALSE, ])
+	}
+	
+
     if (header || !missing(nrows)) {
         if (length(nrows) == 1) nrows <- rep(nrows, length(files))
         for (i in seq_along(files)) {
