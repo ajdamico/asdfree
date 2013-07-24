59ae1b064dddad590aaa7f9bdb301c501bbe9803
Anthony Damico <ajdamico@gmail.com>
2013-07-24 17:34:09 -0400

    1st addhealth

---
+++ National Longitudinal Study of Adolescent Health/download and consolidate.R
@@ -0,0 +1,366 @@
+# analyze survey data for free (http://asdfree.com) with the r language
+# national longitudinal study of adolescent health
+# waves 1 through 4, interviews 1994 - 2008
+
+# if you have never used the r language before,
+# watch this two minute video i made outlining
+# how to run this script from start to finish
+# http://www.screenr.com/Zpd8
+
+# anthony joseph damico
+# ajdamico@gmail.com
+
+# if you use this script for a project, please send me a note
+# it's always nice to hear about how people are using this stuff
+
+# for further reading on cross-package comparisons, see:
+# http://journal.r-project.org/archive/2009-2/RJournal_2009-2_Damico.pdf
+
+
+###########################################################################################################
+# download every file from every year of the National Longitudinal Study on Adolescent Health with R then #
+# create a consolidated file and save everything as an R data frame (.rda) for rapid  future analyses.    #
+###########################################################################################################
+
+
+# # # # # # # # # # # # # #
+# important user warning! #
+# # # # # # # # # # # # # #
+
+# you *must* visit this university of michigan
+# website and create a username and password
+# before running this massive download automation program
+
+# this is to protect both yourself and the respondents of the study
+# http://www.icpsr.umich.edu/cgi-bin/bob/terms2?study=21600
+# as a verification that you have actually read this document, you must uncomment
+# (meaning remove the # in front of) the next line.  by uncommenting this line, you affirm you have read and agree with the document:
+
+# terms <- "http://www.icpsr.umich.edu/cgi-bin/terms"
+
+# this massive ftp download automation script will not work without the above line uncommented.
+# if the 'terms' line above is still uncommented, the script is going to break.
+# to repeat.  read the important user warning.  then uncomment that line to affirm you have read and agree with the document.
+# finally, place your username and password in the new variables here:
+
+# you're going to want to uncomment these as well.
+# login <- "your@email.com"
+# pw <- "password_here"
+# just remove the `#` in front of those two lines,
+# after you've entered your username and password, uhcourse.
+
+# # # # # # # # # # # # # # # # # 
+# end of important user warning #
+# # # # # # # # # # # # # # # # #
+
+
+# set your working directory.
+# each year of the AddHealth will be stored in a year-specific folder here
+# after downloading and importing it.
+# use forward slashes instead of back slashes
+
+# uncomment this line by removing the `#` at the front..
+# setwd( "C:/My Directory/AddHealth/" )
+# ..in order to set your current working directory
+
+
+
+# remove the # in order to run this install.packages line only once
+# install.packages( "RCurl" )
+
+# no need to edit anything below this line #
+
+
+# # # # # # # # #
+# program start #
+# # # # # # # # #
+
+
+require(foreign) 		# load foreign package (converts data files into R)
+require(RCurl)			# load RCurl package (downloads https files)
+
+# create a few vectors (one numeric, one list, one character) containing all of the data set information
+# on the icpsr website.  each of these numbers correspond to a certain data set on the university of michigan's page.
+# i got 'em from http://www.cpc.unc.edu/projects/addhealth/data/publicdata/public-use-dataset-descriptions
+
+# wave of each data file
+wave = c( 1 , 1 , 1 , 1 , 1 , 2 , 2 , 2 , 3 , 3 , 3 , 3 , 3 , 4 , 4 , 4 , 4 )
+
+# data set number of each data file(s)
+# note that this is a `list` class because multiple data set numbers need to be combined into a single data set
+ds.number = list( 1 , 2 , 19 , 17 , 18 , 3 , 21 , 20 , 4:12 , 22 , 14 , 15 , 16 , 23:28 , 29 , 30 , 31 )
+
+# data set names
+filename = c( "Main" , "Grand Sample Weights" , "Contextual" , "School Weights" , "Network" , "Main" , "Grand Sample Weights" , "Contextual" , "In-Home Questionnaire" , "Grand Sample Weights" , "Education" , "Graduation" , "Education" , "Main" , "Grand Sample Weights" , "Biomarkers" , "Biomarkers" )
+
+
+# webscraping setup #
+
+# start a curl handle
+curl = getCurlHandle()
+
+# set a few preferences for that curl handle
+curlSetOpt(
+	cookiejar = 'cookies.txt' , 
+	followlocation = TRUE , 
+	autoreferer = TRUE , 
+	curl = curl
+)
+
+# login to the icpsr umich website,
+# using the login and password that you should've
+# provided at the start of this script
+login.page <- 
+	postForm(
+		"http://www.icpsr.umich.edu/ticketlogin" , 
+		email = login ,
+		password = pw ,
+		path = "ICPSR" ,
+		request_uri = "http://www.icpsr.umich.edu/cgi-bin/bob/terms2?study=21600&amp;ds=1&amp;bundle=stata&amp;path=ICPSR" ,
+		style = "POST" ,
+		curl = curl 
+	)
+
+# automatically agree to the university of michigan's
+# terms and conditions for using this data.
+# did you read the agreement?  great thanx
+terms.of.use.page <- 
+	postForm(
+		terms , 
+		agree = 'yes' ,
+		path = "ICPSR" , 
+		study = "21600" , 
+		ds = "1" , 
+		bundle = "stata" , 
+		dups = "yes" ,
+		style = "POST" ,
+		curl = curl 
+	)
+# end of webscraping setup #
+	
+# create a temporary file
+tf <- tempfile()
+
+# initiate an empty object that will be filled later
+all.downloaded.rdas <- NULL
+
+# create a directory inside your current working directory
+# that will contain the individual data sets for all of the files
+# that you're about to download.
+dir.create( "individual tables" )
+
+# loop through every number in the `wave` object (created above)
+for ( i in seq( length( wave ) ) ){
+
+	# loop through every object in the current position in
+	# the`ds.number` list (also created above).
+	# note that *most but not all* of the elements of this list are singular
+	# but a few of 'em have multiple objects, all of which need to be downloaded.
+	for ( ds in ds.number[[i]] ){
+
+		# print the current progress to the screen
+		print( paste( "currently working on w" , wave[i] , '-' , filename[ i ] , '-' , ds ) )
+	
+		# design the full filepath of the stata data set to download
+		fp <- paste0( "http://www.icpsr.umich.edu/cgi-bin/bob/zipcart2?path=ICPSR&study=21600&bundle=stata&ds=" , ds , "&dups=yes" )
+	
+		# download the current stata file into working memory
+		file <- getBinaryURL( fp , curl = curl )
+
+		# save the current downloaded file to the temporary file on the hard disk
+		writeBin( file , tf )
+
+		# unzip that file..
+		all.files <- unzip( tf )
+		
+		# find the `.dta` within the zip
+		stata.file <- all.files[ grep( ".dta" , all.files , fixed = TRUE ) ]
+
+		# double-check that there's only one `.dta`
+		if ( length( stata.file ) > 1 ) stop( "should only be one dta per zip" )
+		
+		# read the stata file into an R data.frame
+		x <- read.dta( stata.file , convert.factors = FALSE )
+
+		# convert all column names to lowercase
+		names( x ) <- tolower( names( x ) )
+		
+		# identify the `.do` file from among the unzipped files
+		do.file <- all.files[ grep( ".do" , all.files , fixed = TRUE ) ]
+
+		# double-check yet again
+		if ( length( do.file ) > 1 ) stop( "should only be one do file per zip" )
+		
+		# if and only if there's one `.do` file..
+		if ( length( do.file ) == 1 ){
+			
+			# convert all characters to lowercase
+			do.lines <- tolower( readLines( do.file ) )
+			
+			# identify all `replace` lines (a stata command)
+			replace.lines <- do.lines[ substr( do.lines , 1 , 7 ) == "replace" ]
+			
+			# split each of those lines at the text `if `
+			both.if.sides <- strsplit( replace.lines , "if " )
+			
+			# extract the text *after* (2) the `if `
+			missing.conditions <- lapply( both.if.sides , "[[" , 2 )
+			
+			# extract the text *before* (1) the `if `
+			vars.to.replace <- lapply( both.if.sides , "[[" , 1 )
+			
+			# remove the word `replace `
+			vars.to.replace <- gsub( "replace " , "" , vars.to.replace )
+			
+			# remove the stata *set equal to missing* text
+			vars.to.replace <- gsub( " = . " , "" , vars.to.replace , fixed = TRUE )
+			
+			# loop through each variable to replace..
+			for ( j in seq( length( vars.to.replace ) ) ){
+			
+				# find the logical conditions that merit conversion to missings
+				replacement.condition <- missing.conditions[[j]]
+				
+				# extract the current variable (column) name
+				curvar <- vars.to.replace[j]
+				
+				# extract the stata rows that ought to be replaced into a new object `z`
+				z <- rownames( subset( x , eval( parse( text = replacement.condition ) ) ) )
+				
+				# and wherever the `x` data.frame matches one of those values,
+				# set the current variable to missing.
+				x[ rownames( x ) %in% z , curvar ] <- NA
+				
+			}
+		}
+			
+		
+		# figure out the appropriate name for the R data file (.rda) on the local disk
+		savename <- paste0( './individual tables/wave ' , wave[i] , ' - ' , filename[ i ] , ' - ' , ds , '.rda' )
+		
+		# save the object `x`
+		save( x , file = savename )
+
+		# add that `.rda` to the character vector of all downloaded rda files
+		all.downloaded.rdas <- c( all.downloaded.rdas , savename )
+		
+		# remove the objects `x` and `file` from working memory
+		rm( x , file )
+		
+		# clear up RAM
+		gc()
+		
+		# remove all unzipped files from the local disk
+		unlink( all.files , recursive = TRUE )
+		
+	}
+}
+
+
+# loop through each of the available interview waves..
+for ( curWave in unique( wave ) ){
+
+	# extract the `.rda` files available for that wave
+	rda.files.to.merge.this.wave <- all.downloaded.rdas[ grepl( paste( 'wave' , curWave ), all.downloaded.rdas ) ]
+
+	# create an empty `cons` object
+	cons <- NULL	
+	
+	# loop through each of the appropriate `.rda` files
+	for ( cur.rda in rda.files.to.merge.this.wave ){
+	
+		# load it into RAM
+		load( cur.rda )
+		
+		# confirm the file must be one-record-per-unique ID
+		if ( length( unique( x$aid ) ) == nrow( x ) ){
+		
+			# print current progress to the screen
+			print( paste( "currently merging" , cur.rda , "from wave" , curWave ) )
+	
+			# if it's the Main file, throw out the cluster2 variable
+			# since the weights file has it with the appropriate number of decimals
+			if ( grepl( 'Main' , cur.rda ) ) x$cluster2 <- NULL
+	
+			# if the `cons` object is missing..
+			if ( is.null( cons ) ){
+				
+				# it's the first data.frame to be included in the consolidated file
+				cons <- x
+			
+			# otherwise
+			} else {
+			
+				# copy over what's already in the `cons` object
+				pre.cons <- cons
+				
+				# if the unique identifier is available,
+				# don't also merge on caseid.
+				if ( 'aid' %in% names( cons ) ) cons$caseid <- NULL
+				
+				# print what you're doing, just to keep everyone abreast of current inner-workings.
+				print( paste( "merging with" , intersect( names( x ) , names( cons ) ) , collapse = " and " ) )
+				
+				# merge the current .rda with what's already in `cons`,
+				# keeping matching records in *either* data set
+				cons <- merge( cons , x , all = TRUE )
+				
+			}
+			
+			# make sure the many-to-one merge hasn't gone apeshit.
+			# none of these should have more than ten thousand records ever
+			stopifnot( nrow( cons ) < 10000 )
+			
+		} else {
+		
+			# otherwise no merge..
+			print( paste( "did not merge" , cur.rda , " -- copying to working directory" ) )
+			
+			# just save the data.frame object into the main output folder
+			save( x , file = gsub( "/individual tables" , "" , cur.rda ) )
+		}
+		
+		# remove the current data.frame from working memory
+		rm( x )
+		
+		# clear up RAM
+		gc()
+	}
+	
+	# once you've merged as many files as you can,
+	# save the final `cons` object to the local disk
+	save( cons , file = paste0( 'wave ' , curWave , ' consolidated.rda' ) )
+	
+	# remove the `cons` object from working memory
+	rm( cons )
+	
+	# once again, clear up RAM
+	gc()
+}
+
+# the current working directory should now contain one consolidated file
+# *plus* all tables that were not one-record-per-id for each wave
+
+
+# once complete, this script does not need to be run again.
+# instead, use one of the analysis scripts,
+# which utilize these newly-created R data files (.rda)
+
+
+# print a reminder: set the directory you just saved everything to as read-only!
+message( paste( "all done.  you should set" , getwd() , "read-only so you don't accidentally alter these files." ) )
+
+
+# for more details on how to work with data in r
+# check out my two minute tutorial video site
+# http://www.twotorials.com/
+
+# dear everyone: please contribute your script.
+# have you written syntax that precisely matches an official publication?
+message( "if others might benefit, send your code to ajdamico@gmail.com" )
+# http://asdfree.com needs more user contributions
+
+# let's play the which one of these things doesn't belong game:
+# "only you can prevent forest fires" -smokey bear
+# "take a bite out of crime" -mcgruff the crime pooch
+# "plz gimme your statistical programming" -anthony damico
---
+++ National Longitudinal Study of Adolescent Health/replicate unc puf.R
@@ -0,0 +1,269 @@
+# analyze survey data for free (http://asdfree.com) with the r language
+# national longitudinal study of adolescent health
+# wave 1 public use file replication
+
+# if you have never used the r language before,
+# watch this two minute video i made outlining
+# how to run this script from start to finish
+# http://www.screenr.com/Zpd8
+
+# anthony joseph damico
+# ajdamico@gmail.com
+
+# if you use this script for a project, please send me a note
+# it's always nice to hear about how people are using this stuff
+
+# for further reading on cross-package comparisons, see:
+# http://journal.r-project.org/archive/2009-2/RJournal_2009-2_Damico.pdf
+
+# note that the university of north carolina's carolina population center only publishes standard error examples
+# using the restricted access data files (shown here):
+stop( 'pdf page 18 of wt_guidelines.pdf' )
+# so to confirm that the methodology below is correct, i asked the folks at unc..
+stop( 'my e-mail to unc on my github' )
+# ..to provided me some example stata output run on the public use file (puf).  they did.  radical.
+stop( 'stata output on my github' )
+# this r script will replicate the statistics from that custom run of the national longitudinal study of adolescent health (addhealth) exactly
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#################################################################################################################################################
+# prior to running this replication script, at least wave 1 of the addhealth public use microdata files must be loaded as R data      #
+# files (.rda) on the local machine. running the "download and consolidate.R" script will create these files.                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+stop( 'fix this' )
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# that script will save a number of .rda files in C:/My Directory/AddHealth/ (or the working directory chosen)                        #
+#################################################################################################################################################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+#######################################################################
+# Analyze the National Longitudinal Study of Adolescent Health with R #
+#######################################################################
+
+
+# set your working directory.
+# the wave 1 AddHealth consolidated file should have been stored within this folder
+# so if "wave 1 consolidated.rda" exists in the directory "C:/My Directory/AddHealth/"
+# then the working directory should be set to "C:/My Directory/AddHealth/"  easy.
+# use forward slashes instead of back slashes
+
+# uncomment this line by removing the `#` at the front..
+# setwd( "C:/My Directory/AddHealth/" )
+# ..in order to set your current working directory
+
+
+# remove the # in order to run this install.packages line only once
+# install.packages( "survey" )
+
+# no need to edit anything below this line #
+
+# # # # # # # # #
+# program start #
+# # # # # # # # #
+
+
+require(survey)  # load survey package (analyzes complex design surveys)
+
+
+# by default, R will crash if a primary sampling unit (psu) has a single observation
+# set R to produce conservative standard errors instead of crashing
+# http://faculty.washington.edu/tlumley/survey/exmample-lonely.html
+# by keeping this line uncommented:
+options( survey.lonely.psu = "adjust" )
+# this setting matches the MISSUNIT option in SUDAAN
+
+
+# the r data frame can be loaded directly from your local hard drive
+load( "wave 1 consolidated.rda" )
+
+
+# display the number of rows in the wave 1 consolidated data set
+nrow( cons )
+
+# display the first six records in the wave 1 data set
+head( cons )
+# note that the data frame contains far too many variables to be viewed conveniently
+
+# create a character vector that will be used to
+# limit the file to only the variables needed
+KeepVars <-
+	c( 
+		"gswgt1" , 		# wave 1 cross-sectional analytic weight
+		
+		"cluster2" , 	# primary sampling units
+		
+		"h1gi1y" ,		# year of birth
+		
+		"h1gi1m" ,		# month of birth
+		
+		"iyear" ,		# interview year
+		
+		"imonth" , 		# interview month
+		
+		"iday" , 		# interview day
+		
+		"bio_sex" ,		# male/female
+		
+		"ah_pvt" ,		# vocabulary test score
+		
+		"h1da8"			# hours of television watched
+	)
+
+
+# limit the r data frame (cons) containing all variables
+# to a severely-restricted r data frame containing only the ten variables
+# specified in character vector 'KeepVars'
+x <- cons[ , KeepVars ]
+
+# to free up RAM, remove the full r data frame
+rm( cons )
+
+# garbage collection: clear up RAM
+gc()
+
+# calculate the birth date, interview date, and `boy` variables
+# to match the stata code used by unc
+x <-
+	transform(
+		x ,
+		
+		# paste the year and month of birth together into a string,
+		# (just set everyone's day of birth to the 15th of the month)
+		# and immediately convert that string to a `date` class
+		birthdate = as.Date( paste0( "19" , h1gi1y , "-" , h1gi1m , "-15" ) ) ,
+		
+		# paste the year-month-day of the interview all together into a string
+		# and likewise, make it a date variable.
+		w1intdate = as.Date( paste0( "19" , iyear , "-" , imonth , "-" , iday ) ) ,
+		
+		# make a `boy` true or false flag.
+		boy = ( bio_sex == 1 )
+		# really that simple
+		
+	)
+
+# calculate the age-at-wave-1-interview
+# by subtracting the interview date from the birth date,
+# dividing by 365.25 (don't forget your leap years)
+# and then converting the column type to an integer.
+cons$w1age <- as.integer( ( cons$w1intdate - cons$birthdate ) / 365.25 )
+
+# at this point, we've got all the variables we need to replicate unc's example stata output	
+	
+#################################################
+# survey design for taylor-series linearization #
+#################################################
+
+# create a survey design object (y) with AddHealth design information
+y <- 
+	svydesign( 
+		id = ~cluster2 , 
+		data = x , 
+		weights = ~gswgt1 , 
+		nest = TRUE 
+	)
+	
+
+#####################
+# analysis examples #
+#####################
+
+# count the total (unweighted) number of records in the wave 1 national longitudinal study of adolescent health data set #
+
+# the nrow function which works on both data frame objects..
+class( x )
+# ..and survey design objects
+class( y )
+
+
+# notice that the original data frame contains the same number of records as..
+nrow( x )
+
+# ..the survey object
+nrow( y )
+
+
+# add a new variable 'one' that simply has the number 1 for each record #
+# and can be used to calculate unweighted and weighted population sizes #
+
+y <-
+	update( 
+		one = 1 ,
+		y
+	)
+
+
+#################################################################
+# print the exact contents of the samhsa document to the screen #
+#################################################################
+
+stop( 'replace this with the stata example.txt link!' )
+# https://github.com/ajdamico/usgsd/blob/master/National%20Survey%20on%20Drug%20Use%20and%20Health/NSDUH%20PUF_Table_1.19B_D%20from%20SAMHSA.pdf?raw=true #
+
+
+# time spent watching tv #
+
+# unweighted counts #
+# (sample size column) #
+unwtd.count( ~h1da8 , y )
+
+# weighted counts #
+# (population size column) #
+svytotal( ~one , y )			# total valid cases
+
+# degrees of freedom for the survey design object
+degf( y )
+
+# weighted mean with standard error
+svymean( ~h1da8 , y , na.rm = TRUE )
+
+# alternatively, save this result into another object `z`
+z <- svymean( ~h1da8 , y , na.rm = TRUE )
+
+# ..and specifically query just the coefficient..
+coef( z )
+
+# ..or just the standard error..
+SE( z )
+
+# ..or just the confidence interval.
+confint( z )
+# what's that, you say?  the confidence interval doesn't precisely match stata?
+# that's because stata uses the degrees of freedom of the survey object
+confint( z , df = degf( y ) )
+# as opposed to r's default, df = Inf
+
+
+# regression time. #
+
+# is performance on the vocab test related to
+# age, sex, and/or time spent watchin' the teevee?
+
+# here's a simple regression..
+svyglm( ah_pvt ~ w1age + boy + h1da8 , y )
+
+# ..but you might want more detail, so put it inside the summary function.
+summary( svyglm( ah_pvt ~ w1age + boy + h1da8 , y ) )
+
+
+########################################################################
+# end of printing the exact contents of the unc document to the screen #
+########################################################################
+
+
+# for more details on how to work with data in r
+# check out my two minute tutorial video site
+# http://www.twotorials.com/
+
+# dear everyone: please contribute your script.
+# have you written syntax that precisely matches an official publication?
+message( "if others might benefit, send your code to ajdamico@gmail.com" )
+# http://asdfree.com needs more user contributions
+
+# let's play the which one of these things doesn't belong game:
+# "only you can prevent forest fires" -smokey bear
+# "take a bite out of crime" -mcgruff the crime pooch
+# "plz gimme your statistical programming" -anthony damico
