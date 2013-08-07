5214271c199db97d12936da5a28cda86453d0834
Anthony Damico <ajdamico@gmail.com>
2013-08-06 21:18:06 -0400

    psid first draft

---
+++ Panel Study of Income Dynamics/download all microdata.R
@@ -0,0 +1,233 @@
+# analyze survey data for free (http://asdfree.com) with the r language
+# panel study of income dynamics
+# 1968 through 2011
+# family, marriage history, childbirth & adoption history, parent identification, cross-year individual
+
+# if you have never used the r language before,
+# watch this two minute video i made outlining
+# how to run this script from start to finish
+# http://www.screenr.com/Zpd8
+
+# anthony joseph damico
+# ajdamico@gmail.com
+
+# if you use this script for a project, please send me a note
+# it's always nice to hear about how people are using this stuff
+
+# for further reading on cross-package comparisons, see:
+# http://journal.r-project.org/archive/2009-2/RJournal_2009-2_Damico.pdf
+
+
+####################################################################################
+# download every file from every year of the Panel Study of Income Dynamics with R #
+# then save every file as an R data frame (.rda) so future analyses can be rapid.  #
+####################################################################################
+
+
+# # # # # # # # # # # # # #
+# important user warning! #
+# # # # # # # # # # # # # #
+
+# you *must* visit this university of michigan's institute for social research
+# website and register for a username and password, then click the link in the e-mail
+# to activate your account before running this massive download automation program
+
+# this is to protect both yourself and the respondents of the study.  register here:
+# http://simba.isr.umich.edu/U/ca.aspx
+# by registering, you are agreeing to the conditions of use on that page
+
+# once you have registered, place your username and password in the script below.
+# this script will not run until a valid username and password are included in the two lines below.
+# and make sure you uncomment those two lines by removing the `#`s as well
+
+# your.username <- 'your@login.com'
+# your.password <- 'yourpassword'
+
+# this massive ftp download automation script will not work without the above lines filled in.
+# if the your.username and your.password lines above are not filled in with the details you provided at registration, 
+# the script is going to break.  to repeat.  register to access psid data.
+
+
+# # # # # # # # # # # # # # # # # 
+# end of important user warning #
+# # # # # # # # # # # # # # # # #
+
+
+# set your working directory.
+# all PSID data files will be stored here
+# after downloading.
+# use forward slashes instead of back slashes
+
+# uncomment this line by removing the `#` at the front..
+# setwd( "C:/My Directory/PSID/" )
+# ..in order to set your current working directory
+
+
+
+# remove the # in order to run this install.packages line only once
+# install.packages( c( "SAScii" , "RCurl" ) )
+
+# no need to edit anything below this line #
+
+
+# # # # # # # # #
+# program start #
+# # # # # # # # #
+
+
+require(SAScii) 	# load the SAScii package (imports ascii data with a SAS script)
+require(RCurl)		# load RCurl package (downloads https files)
+
+
+# follow the authentication technique described on this stackoverflow post
+# http://stackoverflow.com/questions/15853204/how-to-login-and-then-download-a-file-from-aspx-web-pages-with-r
+
+
+# initiate and then set a curl handle to store information about this download
+curl = getCurlHandle()
+
+curlSetOpt(
+	cookiejar = 'cookies.txt' , 
+	followlocation = TRUE , 
+	autoreferer = TRUE , 
+	curl = curl
+)
+
+# connect to the login page to download the contents of the `viewstate` option
+html <- 
+	getURL(
+		'http://simba.isr.umich.edu/u/Login.aspx' , 
+		curl = curl
+	)
+
+# extract the `viewstate` string
+viewstate <- 
+	as.character(
+		sub(
+			'.*id="__VIEWSTATE" value="([0-9a-zA-Z+/=]*).*' , 
+			'\\1' , 
+			html
+		)
+	)
+
+# construct a list full of parameters to pass to the umich website
+params <- 
+	list(
+		'ctl00$ContentPlaceHolder3$Login1$UserName'    = your.username ,
+		'ctl00$ContentPlaceHolder3$Login1$Password'    = your.password ,
+		'ctl00$ContentPlaceHolder3$Login1$LoginButton' = 'Log In' ,
+		'__VIEWSTATE'                                  = viewstate
+    )
+# and now, with the username, password, and viewstate parameters all squared away
+# it's time to start downloading individual files from the umich website	
+
+
+# # # # # # # # # # # # #
+# custom save function  #
+
+# initiate a function that requires..
+save.psid <-
+	# ..a file number, a save name, the parameters list, and the curl options
+	function( file , name , params , curl ){
+
+		# logs into the umich form
+		html = postForm('http://simba.isr.umich.edu/u/Login.aspx', .params = params, curl = curl)
+		
+		# confirms the result's contents contains the word `Logout` because
+		# if it does not contain this text, you're not logged in.  sorry.
+		if ( !grepl('Logout', html) ) stop( 'no longer logged in' )
+	
+		# initiate a temporary file and a temporary directory
+		tf <- tempfile() ; td <- tempdir()
+		
+		# download the file number
+		file <- 
+			getBinaryURL( 
+				paste0( 
+					"http://simba.isr.umich.edu/Zips/GetFile.aspx?file=" , 
+					file 
+				) , 
+				curl = curl 
+			)
+		
+		# write the file to the temporary file on the local disk
+		writeBin( file , tf )
+		
+		# unzip the temporary file to the temporary directory
+		z <- unzip( tf , exdir = td )
+	
+		# figure out which file contains the data (so no readmes or technical docs)
+		fn <- z[ grepl( ".txt" , tolower( z ) , fixed = TRUE ) & ! grepl( "_vdm|readme|doc|errata" , tolower( z ) ) ]
+		
+		# figure out which file contains the sas importation script
+		sas_ri <- z[ grepl( '.sas' , z , fixed = TRUE ) ]
+
+		# read the text file directly into an R data frame with `read.SAScii`
+		x <- read.SAScii( fn , sas_ri )
+
+		# convert all column names to lowercase
+		names( x ) <- tolower( names( x ) )
+		
+		# remove the files you'd downloaded from the local disk
+		file.remove( tf , z )
+	
+		# copy the data.frame `x` over to whatever the `name` parameter was supposed to be
+		assign( name , x )
+		
+		# save the renamed data.frame to an R data file (.rda)
+		save( list = name , file = paste0( name , '.rda' ) )
+		
+		# delete the data.frame `x`
+		rm( x )
+
+		# delete the data.frame (name)
+		rm( list = name )
+		
+		# clear up RAM
+		gc()
+		
+		# confirm that the function worked by returning TRUE
+		TRUE
+	}
+		
+# end of custom save function #
+# # # # # # # # # # # # # # # #
+
+
+# construct a `family` data.frame of all file names specified on
+# http://simba.isr.umich.edu/Zips/ZipMain.aspx
+# (note: you may have to be logged in to see those files)
+family <-
+	data.frame(
+		year = c( 1968:1997 , seq( 1999 , 2011 , 2 ) ) ,
+		file = c( 1056 , 1058:1082 , 1047:1051 , 1040 , 1052 , 1132 , 1139 , 1152 , 1156 )
+	)
+
+# loop through each record in the `family` file..
+for ( i in seq( nrow( family ) ) ){
+	
+	# ..and save the file to the local disk, using the pre-defined function
+	save.psid( family[ i , 'file' ] , paste0( "fam" , family[ i , 'year' ] ) , params , curl )
+}
+
+# automatically down the marriage, childbirth, parentid, and cross-year individual files
+# the exact same way, with the exact samee pre-defined function
+save.psid( 1121 , 'marriage' , params , curl )
+save.psid( 1109 , 'childbirth' , params , curl )
+save.psid( 1123 , 'parentid' , params , curl )
+save.psid( 1053 , 'ind' , params , curl )
+
+
+# for more details on how to work with data in r
+# check out my two minute tutorial video site
+# http://www.twotorials.com/
+
+# dear everyone: please contribute your script.
+# have you written syntax that precisely matches an official publication?
+message( "if others might benefit, send your code to ajdamico@gmail.com" )
+# http://asdfree.com needs more user contributions
+
+# let's play the which one of these things doesn't belong game:
+# "only you can prevent forest fires" -smokey bear
+# "take a bite out of crime" -mcgruff the crime pooch
+# "plz gimme your statistical programming" -anthony damico
---
+++ Panel Study of Income Dynamics/replicate umich.R
@@ -0,0 +1,189 @@
+# analyze survey data for free (http://asdfree.com) with the r language
+# panel study of income dynamics
+# replication of umich statistics
+
+# note that the folks at the panel study of income dynamics claim there's no "recommended" variance calculation
+# so i asked one of the authors of this paper -
+# http://psidonline.isr.umich.edu/Publications/Papers/tsp/2011-05_Heeringa_Berglung_Khan.pdf
+# - to provide me with example sas output that i could precisely match.  they did.  here it is:
+# https://raw.github.com/ajdamico/usgsd/master/Panel%20Study%20of%20Income%20Dynamics/umich%20output.pdf?raw=TRUE
+# and this script matches those results exactly.  but it only works exactly on the 1968-2009 individual cross-year file.
+# the 1968-2009 individual cross-year file is already obsolete, so if you run this script,
+# you'll get every-so-slightly different numbers.  to assuage your concerns, i've run the whole script on the 1968-2009 file
+# and saved the output here:
+# https://raw.github.com/ajdamico/usgsd/master/Panel%20Study%20of%20Income%20Dynamics/replication%20output.pdf?raw=TRUE
+# hope that works for you.  if it doesn't, e-mail psidhelp@umich.edu and ask for the 1968-2009 file yourself ;)
+
+# if you have never used the r language before,
+# watch this two minute video i made outlining
+# how to run this script from start to finish
+# http://www.screenr.com/Zpd8
+
+# anthony joseph damico
+# ajdamico@gmail.com
+
+# if you use this script for a project, please send me a note
+# it's always nice to hear about how people are using this stuff
+
+# for further reading on cross-package comparisons, see:
+# http://journal.r-project.org/archive/2009-2/RJournal_2009-2_Damico.pdf
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#####################################################################################################################
+# prior to running this analysis script, the umich individual cross-year file must be downloaded to your local disk #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# https://raw.github.com/ajdamico/usgsd/master/Panel%20Study%20of%20Income%20Dynamics/download%20all%20microdata.R  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# that script will place all necessary psid files whever you specified, probably the "C:/My Directory/PSID/" folder #
+#####################################################################################################################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+#####################################################################################################
+# replicate example output provided by the authors of the psid's design-based sampling error report #
+#####################################################################################################
+
+
+# set your working directory.
+# the R data file (.rda) should have been stored within this folder
+# use forward slashes instead of back slashes
+
+# uncomment this line by removing the `#` at the front..
+# setwd( "C:/My Directory/PSID/" )
+# ..in order to set your current working directory
+
+
+# remove the # in order to run this install.packages line only once
+# install.packages( "survey" )
+
+
+
+# no need to edit anything below this line #
+
+
+# # # # # # # # #
+# program start #
+# # # # # # # # #
+
+require(survey)		# load survey package (analyzes complex design surveys)
+
+
+# load the individual cross-year file
+load( "ind.rda" )
+
+
+# limit the file to only the variables needed
+KeepVars <-
+	c(
+		"er32000" ,		# sex
+		"er34020" , 	# education level
+		"er31997" ,		# primary sampling unit
+		"er31996" ,		# strata
+		"er34046"		# weights
+	)
+
+	
+# create a "skinny"	data.frame object that only contains the
+# columns you need for this analysis,
+# specified in character vector 'KeepVars'
+x <- ind[ , KeepVars ]
+
+
+# to free up memory, remove the full r data frame
+rm( ind )
+
+# clear up RAM
+gc()
+
+
+# perform all recodes on the `individual` table #
+
+# create a `completed_ed` column that simply blanks out
+# `er34020` values of 98 or 99, but otherwise uses
+# whatever's already in `er34020`
+x <- 
+	transform( 
+		x , 
+		completed_ed = ifelse( er34020 %in% 98:99 , NA , er34020 ) 
+	)
+
+# end of all recodes #
+
+# create survey design object with PSID design information
+y <- 
+	svydesign( 
+		~er31997 , 
+		strata = ~er31996 , 
+		data = x , 
+		weights = ~er34046 , 
+		nest = TRUE 
+	)
+
+# extract the unweighted available number of observations
+unwtd.count( ~completed_ed , y )
+
+# calculate the mean of the `completed_ed` column created above..
+c_ed <- svymean( ~completed_ed , y , na.rm = TRUE )
+# ..to create a `svystat` object `c_ed` 
+class( c_ed )
+
+# extract the actual statistic..
+coef( c_ed )
+
+# ..the standard error..
+SE( c_ed )
+
+# ..and confidence intervals, both default..
+confint( c_ed )
+
+# ..and matching sas.
+confint( c_ed , df = degf( y ) )
+
+# run the same query, broken down by the sex variable
+c_ed_by_sex <- 
+	svyby( 
+		~completed_ed , 
+		~er32000 , 
+		y , 
+		svymean , 
+		na.rm = TRUE 
+	)
+
+
+# extract the actual statistic..
+coef( c_ed_by_sex )
+
+# ..the standard error..
+SE( c_ed_by_sex )
+
+# ..and confidence intervals, both default..
+confint( c_ed_by_sex )
+
+# ..and matching sas.
+confint( c_ed_by_sex , df = degf( y ) )
+
+# sum up the counts by gender
+svytotal( ~factor( er32000 ) , y )
+
+# calculate the proportion of each gender,
+# also printing the results to the screen
+( c_sex <- svymean( ~factor( er32000 ) , y ) )
+
+# then, all by its lonesome, print the standard error too.
+SE( c_sex )
+
+
+# for more details on how to work with data in r
+# check out my two minute tutorial video site
+# http://www.twotorials.com/
+
+# dear everyone: please contribute your script.
+# have you written syntax that precisely matches an official publication?
+message( "if others might benefit, send your code to ajdamico@gmail.com" )
+# http://asdfree.com needs more user contributions
+
+# let's play the which one of these things doesn't belong game:
+# "only you can prevent forest fires" -smokey bear
+# "take a bite out of crime" -mcgruff the crime pooch
+# "plz gimme your statistical programming" -anthony damico
