e8bdff93342bfc82e0bc3610f0c99394df77d47f
Anthony Damico <ajdamico@gmail.com>
2013-11-21 09:19:43 -0500

    wave 14

--- Survey of Income and Program Participation/2008 panel - download and create database.R
+++ Survey of Income and Program Participation/2008 panel - download and create database.R
@@ -1,7 +1,7 @@
 # analyze survey data for free (http://asdfree.com) with the r language
 # survey of income and program participation
 # 2008 panel
-# 13 core waves, 13 wave-specific replicate weights, 11 topical modules, 
+# 14 core waves, 14 wave-specific replicate weights, 11 topical modules, 
 # 3 panel year replicate weights, 3 calendar year replicate weights, 1 longitudinal weights
 
 # # # # # # # # # # # # # # # # #
@@ -51,8 +51,8 @@
 
 SIPP.dbname <- "SIPP08.db"																# choose the name of the database (.db) file on the local disk
 
-sipp.core.waves <- 1:13																	# either choose which core survey waves to download, or set to NULL
-sipp.replicate.waves <- 1:13															# either choose which replicate weight waves to download, or set to NULL
+sipp.core.waves <- 1:14																	# either choose which core survey waves to download, or set to NULL
+sipp.replicate.waves <- 1:14															# either choose which replicate weight waves to download, or set to NULL
 sipp.topical.modules <- 1:11															# either choose which topical modules to download, or set to NULL
 sipp.longitudinal.weights <- TRUE														# set to FALSE to prevent download
 sipp.cy.longitudinal.replicate.weights <- paste0( 'cy' , 1:3 )							# reads in 2009-2011
