ee63b45a7c9b1d11e3ceb01f160d68d9a0e1478e
Anthony Damico <ajdamico@gmail.com>
2014-07-15 12:38:06 -0400

    m99 still causing problems

--- United States Decennial Census Public Use Microdata Sample/pums functions.R
+++ United States Decennial Census Public Use Microdata Sample/pums functions.R
@@ -87,7 +87,10 @@ get.tsv <-
 			# ..otherwise, there's something wrong with the file!
 
 			# remove goofy special characters (that will break monetdb)
+			line <- gsub( "äm99" , "  99" , line , fixed = TRUE )
+			
 			line <- gsub( "[^[:alnum:]///' ]" , " " ,  line )
+			
 			line <- iconv( line , "" , "ASCII" , sub = " " )
 			
 			line <- gsub( "P00083710210010540112000012110014100000028401800020193999910000000200000000000000000000000000000000000000p" , "P00083710210010540112000012110014100000028401800020193999910000000200000000000000000000000000000000000000  " , line , fixed = TRUE )
