37f0fa78a894bde201e56ac3316e5ef89ac0f494
Anthony Damico <ajdamico@gmail.com>
2015-11-05 13:27:51 -0500

    one more newline

--- National Household Travel Survey/download and import.R
+++ National Household Travel Survey/download and import.R
@@ -691,7 +691,7 @@ for ( year in years.to.download ){
 
 					# read the comma separated value (csv) file you just downloaded
 					# directly into the monet database you just created.
-					monet.read.csv( db , i , tablename , header = TRUE , nrow.check = 250000 , lower.case.names = TRUE )
+					monet.read.csv( db , i , tablename , header = TRUE , nrow.check = 250000 , lower.case.names = TRUE , newline = '\\r\\n' )
 					# yes.  you did all that.  nice work.
 
 					# delete the csv file from your local disk,
