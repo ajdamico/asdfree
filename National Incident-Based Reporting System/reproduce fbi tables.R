ebb44c1e0827bdeb3518ff4ade0df50e8321be57
Anthony Damico <ajdamico@gmail.com>
2015-12-20 19:20:04 -0500

    nibrs integration

--- National Incident-Based Reporting System/reproduce fbi tables.R
+++ National Incident-Based Reporting System/reproduce fbi tables.R
@@ -5,9 +5,8 @@
 # # # # # # # # # # # # # # # # #
 # # block of code to run this # #
 # # # # # # # # # # # # # # # # #
-# options( "monetdb.sequential" = TRUE )		# # only windows users need this line
 # library(downloader)
-# batfile <- "C:/My Directory/NIBRS/MonetDB/nibrs.bat"		# # note for mac and *nix users: `nibrs.bat` might be `nibrs.sh` instead
+# setwd( "C:/My Directory/NIBRS/" )
 # source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/National%20Incident-Based%20Reporting%20System/reproduce%20fbi%20tables.R" , prompt = FALSE , echo = TRUE )
 # # # # # # # # # # # # # # #
 # # end of auto-run block # #
@@ -20,14 +19,14 @@
 
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#############################################################################################################################
-# prior to running this analysis script, the 2012 national incident-based reporting system files must be imported into a    #
-# a monet database on the local machine. before running the dbGetQuery commands below, you must load the data with this:    #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# https://raw.githubusercontent.com/ajdamico/asdfree/master/National%20Incident-Based%20Reporting%20System/download%20all%20microdata.R  #
-#############################################################################################################################
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#########################################################################################################################################
+# prior to running this analysis script, the 2012 national incident-based reporting system files must be imported into a                #
+# a monet database on the local machine. before running the dbGetQuery commands below, you must load the data with this:                #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# https://raw.githubusercontent.com/ajdamico/asdfree/master/National%20Incident-Based%20Reporting%20System/download%20all%20microdata.R #
+#########################################################################################################################################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
 
@@ -54,62 +53,28 @@
 
 
 
-# # # # # # # # # # # # # # #
-# warning: monetdb required #
-# # # # # # # # # # # # # # #
-
-# windows machines and also machines without access
-# to large amounts of ram will often benefit from
-# the following option, available as of MonetDB.R 0.9.2 --
-# remove the `#` in the line below to turn this option on.
-# options( "monetdb.sequential" = TRUE )		# # only windows users need this line
-# -- whenever connecting to a monetdb server,
-# this option triggers sequential server processing
-# in other words: single-threading.
-# if you would prefer to turn this on or off immediately
-# (that is, without a server connect or disconnect), use
-# turn on single-threading only
-# dbSendQuery( db , "set optimizer = 'sequential_pipe';" )
-# restore default behavior -- or just restart instead
-# dbSendQuery(db,"set optimizer = 'default_pipe';")
+# uncomment this line by removing the `#` at the front..
+# setwd( "C:/My Directory/NIBRS/" )
+# ..in order to set your current working directory
 
 
 # remove the # in order to run this install.packages line only once
 # install.packages( "sqldf" )
 
 
-library(MonetDB.R)	# load MonetDB.R package (creates database files in R)
+library(MonetDB.R)	# load the MonetDB.R package (connects r to a monet database)
+library(MonetDBLite)# load MonetDBLite package (creates database files in R)
 library(sqldf)		# load the sqldf package (enables sql queries on data frames)
 
 
 
-######################################################################
-# lines of code to hold on to for all other `nibrs` monetdb analyses #
+# name the database files in the "MonetDB" folder of the current working directory
+dbfolder <- paste0( getwd() , "/MonetDB" )
 
-# first: specify your batfile.  again, mine looks like this:
-# uncomment this line by removing the `#` at the front..
-# batfile <- "C:/My Directory/NIBRS/MonetDB/nibrs.bat"		# # note for mac and *nix users: `nibrs.bat` might be `nibrs.sh` instead
-
-# second: run the MonetDB server
-monetdb.server.start( batfile )
-
-# third: your five lines to make a monet database connection.
-# just like above, mine look like this:
-dbname <- "nibrs"
-dbport <- 50014
-
-monet.url <- paste0( "monetdb://localhost:" , dbport , "/" , dbname )
-db <- dbConnect( MonetDB.R() , monet.url , wait = TRUE )
-
-# fourth: store the process id
-pid <- as.integer( dbGetQuery( db , "SELECT value FROM env() WHERE name = 'monet_pid'" )[[1]] )
+# open the connection to the monetdblite database
+db <- dbConnect( MonetDBLite() , dbfolder )
 
 
-# lines of code to hold on to for all other `nibrs` monetdb analyses #
-######################################################################
-
-
-# # # # run your analysis commands # # # #
 
 
 # # # # # # # # # # # # #
@@ -291,16 +256,6 @@ dbGetQuery(
 )
 
 
-
-#############################################################################
-# end of lines of code to hold on to for all other `nibrs` monetdb analyses #
-
 # disconnect from the current monet database
 dbDisconnect( db )
-
-# and close it using the `pid`
-monetdb.server.stop( pid )
-
-# end of lines of code to hold on to for all other `nibrs` monetdb analyses #
-#############################################################################
 
