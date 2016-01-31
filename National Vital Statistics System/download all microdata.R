98e839d3b8933c9dfca2578cd3808c763c71c1a0
Anthony Damico <ajdamico@gmail.com>
2016-01-31 18:40:46 -0500

    more files available

--- National Vital Statistics System/download all microdata.R
+++ National Vital Statistics System/download all microdata.R
@@ -9,11 +9,11 @@
 # path.to.winrar <- normalizePath( "C:/Program Files/winrar/winrar.exe" )		# # only windows users need this line
 # library(downloader)
 # setwd( "C:/My Directory/NVSS/" )
-# natality.sets.to.download <- 2013:1999
-# periodlinked.sets.to.download <- 2011:2001
-# cohortlinked.sets.to.download <- 2009:1995
-# mortality.sets.to.download <- 2012:2000
-# fetaldeath.sets.to.download <- 2012:2005
+# natality.sets.to.download <- 2014:1999
+# periodlinked.sets.to.download <- 2013:2001
+# cohortlinked.sets.to.download <- 2010:1995
+# mortality.sets.to.download <- 2014:2000
+# fetaldeath.sets.to.download <- 2013:2005
 # source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/National%20Vital%20Statistics%20System/download%20all%20microdata.R" , prompt = FALSE , echo = TRUE )
 # # # # # # # # # # # # # # #
 # # end of auto-run block # #
