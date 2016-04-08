b8a6d7e56b89de0ccf7baa68b519cc4bf052ab93
Anthony Damico <ajdamico@gmail.com>
2016-04-08 08:33:11 -0400

    ibge renamed a file

--- Pesquisa Nacional por Amostra de Domicilios/download all microdata.R
+++ Pesquisa Nacional por Amostra de Domicilios/download all microdata.R
@@ -193,6 +193,9 @@ for ( year in years.to.download ){
 		
 			if ( year == 2013 & this.file == "Dicionarios_e_input.zip" ) this.file <- "Dicionarios_e_input_20150814.zip"
 		
+			if( year == 2014 ) this.file <- gsub( "\\.zip" , "_20160406.zip" , this.file )
+			
+		
 			try({
 				
 				# give downloading 'em a shot
