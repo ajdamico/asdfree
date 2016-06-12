bca155fe9f7dbacedfca963edb9f94c6a4c1997a
carlganz <carlganz@colorado.edu>
2016-06-12 16:02:22 -0700

    Update CHIS

--- California Health Interview Survey/replication.R
+++ California Health Interview Survey/replication.R
@@ -12,27 +12,17 @@ load( "./2011/adult.rda" )
 
 options( survey.replicates.mse = TRUE )
 
-# footnote (a) says they might have used 0.9999:  http://healthpolicy.ucla.edu/chis/analyze/Documents/2012MAY02-CHIS-PUF-Weighting-and-Variance-2Frequency.pdf
-chis_svy <- svrepdesign( data = x , weights = ~ rakedw0 , repweights = "rakedw[1-9]" , type = "other" , scale = 1 , rscales = 0.9999  , mse = TRUE )
+# http://healthpolicy.ucla.edu/chis/analyze/Documents/2012MAY02-CHIS-PUF-Weighting-and-Variance-2Frequency.pdf
+# consistent with Complex Surveys: a Guide to Analysis in R by Thomas Lumley Chapter #2
+chis_svy <- svrepdesign( data = x , weights = ~ rakedw0 , repweights = "rakedw[1-9]" , type = "other" , scale = 1 , rscales = 1  , mse = TRUE )
 
 
-# compare to 2011 state level estimates in adult health profiles 
-# http://healthpolicy.ucla.edu/health-profiles/adults/Documents/2011/Regions/LosAngeles.pdf
+# compare to 2014 state level estimates for Adults from the AskCHIS web query system
 
-# No usual source of care
-## CI should equal 16.3-18.1
-x <- svymean(~factor(usoc),chis_svy)
-x
-round(100*confint(x,df=degf(chis_svy)),1)
+# Health Status
+hs <- svymean(~factor(ab1),chis_svy)
+round(100*hs,1)
+round(100*confint(hs,df=degf(chis_svy)),1)
 
-# Obesity
-## CI should equal 24.1-26.0
-y <- svymean(~factor(rbmi),chis_svy)
-y
-round(100*confint(y,df=degf(chis_svy)),1)
-
-# Diabetes
-## CI should equal 7.8-8.9
-z <- svymean(~factor(ab22),chis_svy)
-z
-round(100*confint(z,df=degf(chis_svy)),1)
+### AskCHIS output:
+browseURL("http://i.imgur.com/TAQrygz.png")
