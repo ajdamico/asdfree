6e24faea2f07e5cc1a0547bb86adf998cf7bf75f
Anthony Damico <ajdamico@gmail.com>
2016-09-01 01:50:02 -0400

    2015 addition & prior year fixes

--- Consumer Expenditure Survey/download all microdata.R
+++ Consumer Expenditure Survey/download all microdata.R
@@ -95,15 +95,26 @@ for ( year in years.to.download ){
 	# if the year-specific output directory doesn't exist, create it
 	try( dir.create( output.directory ) , silent = T )
 
+	# download all documentation
+	download_cached( paste0( "http://www.bls.gov/cex/" , year , "/csxintvw.pdf" ) , paste0( output.directory , "csxintvw.pdf" ) , mode = 'wb' )
+	download_cached( paste0( "http://www.bls.gov/cex/" , year , "/csxdiary.pdf" ) , paste0( output.directory , "csxdiary.pdf" ) , mode = 'wb' )
+
+	if( year > 2006 ){
+		download_cached( paste0( "http://www.bls.gov/cex/" , year , "/csxintvwdata.pdf" ) , paste0( output.directory , "csxintvwdata.pdf" ) , mode = 'wb' )
+		download_cached( paste0( "http://www.bls.gov/cex/" , year , "/csxdiarydata.pdf" ) , paste0( output.directory , "csxdiarydata.pdf" ) , mode = 'wb' )
+	}
+	
+	# end of documentation download
+	
+	
 	# determine the exact path to the current year of microdata on the bureau of labor statistics ftp site
 	# for each of the four main consumer expenditure public use microdata files
 	intrvw.ftp <- paste0( "http://www.bls.gov/cex/pumd/data/stata/intrvw" , substr( year , 3 , 4 ) , ".zip" )
 	diary.ftp <- paste0( "http://www.bls.gov/cex/pumd/data/stata/diary" , substr( year , 3 , 4 ) , ".zip" )
-	docs.ftp <- paste0( "http://www.bls.gov/cex/pumd/documentation/documentation" , substr( year , 3 , 4 ) , ".zip" )
 	
 	
 	# all three file types to download
-	ttd <- c( "intrvw" , "diary" , "docs" )
+	ttd <- c( "intrvw" , "diary" )
 	
 	
 	# loop through the interview, expenditure, diary, and documentation files and..
