ba2771bd3ff1ddb0f451b4093337ce5204c7b150
Anthony Damico <ajdamico@gmail.com>
2017-09-28 16:58:35 -0400

    scf 2016

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 # Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'sample ces'"
+commit_memo <- "'scf 2016'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
