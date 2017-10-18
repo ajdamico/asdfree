bc2bb1175a1519d136f3d108e661ae63730ed1ef
Anthony Damico <ajdamico@gmail.com>
2017-10-18 10:25:14 -0400

    pnadc 20%

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 # Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'git pull or git clone'"
+commit_memo <- "'pnadc 20%'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
