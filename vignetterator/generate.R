ec535032b585c7511c0542e722250964a3c6f621
Anthony Damico <ajdamico@gmail.com>
2017-11-12 11:46:20 -0500

    enade bump

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'faers bump'"
+commit_memo <- "'enade bump'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
