dc2fbb9759ab1fa7b7a96a7337b283d4e15662f3
Anthony Damico <ajdamico@gmail.com>
2017-11-12 11:13:16 -0500

    faers bump

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'dhs distribute'"
+commit_memo <- "'faers bump'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
