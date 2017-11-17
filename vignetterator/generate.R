3f7e966b8b3bde864104eddceebbc8e103559abb
Anthony Damico <ajdamico@gmail.com>
2017-11-17 03:19:14 -0500

    enade cooldown

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'remove sisyphus badges'"
+commit_memo <- "'enade cooldown'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
