feb93c495ff5c2a6eb061608361ebd42732c3671
Anthony Damico <ajdamico@gmail.com>
2017-12-14 04:55:53 -0500

    nbs fix

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'give pnadc/ibge ftp more time'"
+commit_memo <- "'nbs fix'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
