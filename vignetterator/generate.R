567a3399787ee5417eac4343729af847d7cb6c1c
Anthony Damico <ajdamico@gmail.com>
2018-01-04 16:09:12 -0500

    pnadc sample breaks

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'pnad sample breaks'"
+commit_memo <- "'pnadc sample breaks'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
