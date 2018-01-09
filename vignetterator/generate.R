9dbdd41ca5f3658be418d2afcd943fa7accc2b66
Anthony Damico <ajdamico@gmail.com>
2018-01-09 08:20:08 -0500

    some year sorting

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'travis wait'"
+commit_memo <- "'some year sorting'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
