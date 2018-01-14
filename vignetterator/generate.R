d33591a8f248893dc6ad0347b65879fb21fbef8c
Anthony Damico <ajdamico@gmail.com>
2018-01-14 10:40:10 -0500

    seer

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'acs'"
+commit_memo <- "'seer'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
