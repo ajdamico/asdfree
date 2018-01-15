8cd61561717c9f957682422331488a0e9d344ba1
Anthony Damico <ajdamico@gmail.com>
2018-01-15 07:28:27 -0500

    dhs

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'seer available_files'"
+commit_memo <- "'dhs'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
