f85cac8e1723e60b1d119b966181b00900258f16
Anthony Damico <ajdamico@gmail.com>
2018-01-27 08:12:57 -0500

    new setup sample break

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'acs movement'"
+commit_memo <- "'new setup sample break'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
