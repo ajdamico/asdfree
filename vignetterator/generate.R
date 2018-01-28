f780bf31597430e6dfe38fbd23ba918ba0d68ebd
Anthony Damico <ajdamico@gmail.com>
2018-01-28 11:33:41 -0500

    nsduh

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'new setup sample break'"
+commit_memo <- "'nsduh'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
