f095b35a45fa6e1aa6ebfeab4d44e6830595922b
Anthony Damico <ajdamico@gmail.com>
2018-02-03 09:22:46 -0500

    saeb

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'pnadc'"
+commit_memo <- "'saeb'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
