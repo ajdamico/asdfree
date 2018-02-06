e3a3d4d566509c601f579867cdc19ba0ee83cfd4
Anthony Damico <ajdamico@gmail.com>
2018-02-06 09:25:23 -0500

    columns must match their order in the csv file

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'saeb'"
+commit_memo <- "'columns must match their order in the csv file'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
