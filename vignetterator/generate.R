f76d3c040f1104ecdf5b59971620f6fd0ea38133
Anthony Damico <ajdamico@gmail.com>
2018-02-07 11:06:01 -0500

    censo download test

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'columns must match their order in the csv file'"
+commit_memo <- "'censo download test'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
