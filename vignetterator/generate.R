9e93cd6f8c5c1ea35613e9df7056ea39d64e8e29
Anthony Damico <ajdamico@gmail.com>
2018-02-16 10:18:52 -0500

    nls delim

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'hrs'"
+commit_memo <- "'nls delim'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
