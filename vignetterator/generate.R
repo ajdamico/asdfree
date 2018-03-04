a30dd793b58420ce625c8515aade867f36acfafb
Anthony Damico <ajdamico@gmail.com>
2018-03-04 07:30:46 -0500

    nppes

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'nsch'"
+commit_memo <- "'nppes'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
 # non-survey, not database-backed (ahrf)
