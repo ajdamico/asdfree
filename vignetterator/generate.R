8509dc78e385dc0adf964eee7ec297f690e2a1b5
Anthony Damico <ajdamico@gmail.com>
2018-04-29 10:39:52 -0400

    remove archive from acs

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'binary connections'"
+commit_memo <- "'remove archive from acs'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
