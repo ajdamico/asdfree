73f30ce67b326433f8691521e1dbbd8f8985e740
Anthony Damico <ajdamico@gmail.com>
2018-05-04 10:27:57 -0400

    cpsbasic

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'meps and hrs fixes'"
+commit_memo <- "'cpsbasic'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
