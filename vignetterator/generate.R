2a4d41fda2eb93a85b0ae7f88dfee7d9917cc9fc
Anthony Damico <ajdamico@gmail.com>
2018-05-07 14:09:36 -0400

    addhealth replication

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'ftp cdc gov gsub'"
+commit_memo <- "'addhealth replication'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
