44a167c9e5b22c31070e1e30f63d1cce06a0c6c2
Anthony Damico <ajdamico@gmail.com>
2018-05-07 15:49:14 -0400

    brfss replication

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'addhealth replication'"
+commit_memo <- "'brfss replication'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
