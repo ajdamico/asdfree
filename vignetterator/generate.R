d3f59fc5db80bdde292bea1178ee4c865b44a571
Anthony Damico <ajdamico@gmail.com>
2018-05-13 14:41:41 -0400

    nsch cdc no ftp

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'brfss replication'"
+commit_memo <- "'nsch cdc no ftp'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
