bc75a5afb9b3739dc10f6a7117825ed842b2ee43
Anthony Damico <ajdamico@gmail.com>
2018-07-02 13:33:22 -0400

    share versioning

--- vignetterator/generate.R
+++ vignetterator/generate.R
@@ -1,6 +1,6 @@
 # Sys.getenv("RSTUDIO_PANDOC")
 Sys.setenv("RSTUDIO_PANDOC"="C:/Program Files/RStudio/bin/pandoc")
-commit_memo <- "'cpsbasic'"
+commit_memo <- "'share versioning'"
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 # source( file.path( path.expand( "~" ) , "Github/asdfree/vignetterator/generate.R" ) )
 
